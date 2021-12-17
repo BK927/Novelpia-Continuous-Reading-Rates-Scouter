2c7b9608d1aa1066476417e4b711289c7f3e1994
BK927 <dead4bees927@gmail.com>
2021-12-17 16:01:53 +0900

    선작, 알림 대 조회수 비율 추가, 연독율 부호 버그 수정

--- Novelpia-Continuous-Reading-Rates-Scouter.user.js
+++ Novelpia-Continuous-Reading-Rates-Scouter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         노벨피아 연독률 측정
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.4
 // @description  작가들을 위한 연독율 측정기.
 // @author       BK927
 // @match        https://novelpia.com/novel/*
@@ -30,29 +30,40 @@ window.addEventListener("load", (event) => {
 
 function clacStats() {
     const allEpisodes = document.querySelectorAll("#episode_list > table > tbody > tr");
-    const stats = [];
+    const favorite = parseInt(document.querySelector("#like_text").innerText);
+    const notification = parseInt(document.querySelector("#alarm_text").innerText);
+    const epiStats = [];
 
     allEpisodes.forEach((element, index) => {
         const node = document.querySelector("#episode_list > table > tbody > tr:nth-child(" + (1 + index * 2).toString() + ") > td.font12 > div > font > span:nth-child(2)");
-        const statDict = extractStat(node);
+        const epiStatElement = extractEpisodesStats(node);
         if (index > 0) {
-            const formerView = stats[index - 1]["view"];
-            const numSign = formerView > statDict["view"] ? "-" : "+";
-            const gapPercent = Math.round((Math.abs(formerView - statDict["view"]) / formerView) * 100);
+            const formerView = epiStats[index - 1]["view"];
+            const numSign = formerView > epiStatElement["view"] ? "-" : "+";
+            const gapPercent = Math.round((Math.abs(formerView - epiStatElement["view"]) / formerView) * 100);
             node.innerHTML = node.innerHTML + '<span style="font-size:12px;color:#0000FF;">&nbsp; ' + numSign + gapPercent.toString() + "%</span>";
         }
-        const commentRatio = Math.round((statDict["thumup"] / statDict["comment"]) * 100) / 100;
-        node.innerHTML = node.innerHTML + '<span style="font-size:12px;color:#008000;">&nbsp; 댓글 대 좋아요 비율: ' + commentRatio.toString() + "</span>";
-        stats.push(statDict);
+        const commentRatio = Math.round((epiStatElement["thumup"] / epiStatElement["comment"]) * 100) / 100;
+        node.innerHTML = node.innerHTML + '<span style="font-size:12px;color:#008000;">&nbsp; 댓글/좋아요: ' + commentRatio.toString() + "</span>";
+
+        const thumupRatio = Math.round((epiStatElement["view"] / epiStatElement["thumup"]) * 100) / 100;
+        node.innerHTML = node.innerHTML + '<span style="font-size:12px;color:#008000;">&nbsp; 조회수/좋아요: ' + thumupRatio.toString() + "</span>";
+
+        const favoriteRatio = Math.round((epiStatElement["view"] / favorite) * 100);
+        node.innerHTML = node.innerHTML + '<span style="font-size:12px;color:#FF0000;">&nbsp; 조회수/선작: ' + favoriteRatio.toString() + "%</span>";
+
+        const notificationRatio = Math.round((epiStatElement["view"] / notification) * 100);
+        node.innerHTML = node.innerHTML + '<span style="font-size:12px;color:#FF0000;">&nbsp; 조회수/알림: ' + notificationRatio.toString() + "%</span>";
+
+        epiStats.push(epiStatElement);
     });
 
-    return stats;
+    return epiStats;
 }
 
-function extractStat(element) {
+function extractEpisodesStats(element) {
     const regexp = /[0-9,]+/g;
-    const parsed = element.innerText.match(regexp).map((e) => e.replace(",", ""));
-    //parsed.forEach((e) => console.log(e));
+    const parsed = element.innerText.match(regexp).map((e) => parseInt(e.replace(",", "")));
     const dict = {};
     dict["length"] = parsed[0];
     dict["view"] = parsed[1];
